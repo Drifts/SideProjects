ee25cb1237bbd2b6a30dc1e26a3e58faec3b7420
Drifts <heavyddc@hotmail.com>
2015-03-18 20:32:46 +0000

    Revert "Java and C files"
    This reverts commit 1bf422971b56be4f51d303df8ba48e64538d1c80.

--- DayTime/DayTime.java
+++
@@ -1,9 +0,0 @@
-// DayTime.java
-import java.util.*;
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface DayTime extends Remote
-{
-    public Date getDate() throws RemoteException;
-}
